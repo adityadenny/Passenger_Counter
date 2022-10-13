bfbcc9308dd3ad3f8ee6b86a6db86e41a7b82596
unknown <adityadenny6@gmail.com>
2022-10-13 21:34:47 +0700

    function increment lap

--- index.js
+++ index.js
@@ -20,3 +20,31 @@
 function increment() {
     console.log("The button was clicked")
 }
+
+function myLogger() {
+    console.log(42)
+}
+
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+function myLap(){
+    let totalTime = lap1 + lap2 + lap3
+    console.log(totalTime)
+}
+
+myLap()
+
+let lapsCompleted = 0
+
+function incrementLap() {
+    lapsCompleted = lapsCompleted + 1
+    console.log(lapsCompleted)
+}
+
+incrementLap()
+
+incrementLap()
+
+incrementLap()
