5f869c2cfb14b443cdabfdb43793372a27f738d1
unknown <adityadenny6@gmail.com>
2022-10-13 21:19:26 +0700

    the onclick event listener

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 // document.getElementById("count-el").innerText = 7
 
-let count = 0
+// let count = 0
 
-let bonusPoints = 50
-bonusPoints = bonusPoints + 50
-console.log(bonusPoints)
-bonusPoints = bonusPoints - 75
-console.log(bonusPoints)
-bonusPoints = bonusPoints + 45 
-console.log(bonusPoints)
+// let bonusPoints = 50
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints)
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
+// bonusPoints = bonusPoints + 45 
+// console.log(bonusPoints)
+
+/**
+ * initialize the count as 0
+ * listen for clicks on the increment button
+ * increment the count variable when the button is clicked
+ * change the count-el in the HTML to reflect the new count
+ */
+
+function increment() {
+    console.log("The button was clicked")
+}
