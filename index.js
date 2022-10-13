7bb410a8540a4dae6081425df26ce825446afc5a
unknown <adityadenny6@gmail.com>
2022-10-14 06:58:28 +0700

    error document get element by id

--- index.js
+++ index.js
@@ -10,13 +10,8 @@
 // bonusPoints = bonusPoints + 45 
 // console.log(bonusPoints)
 
-/**
- * initialize the count as 0
- * listen for clicks on the increment button
- * increment the count variable when the button is clicked
- * change the count-el in the HTML to reflect the new count
- */
 
+/**
 function increment() {
     console.log("The button was clicked")
 }
@@ -48,3 +43,24 @@ incrementLap()
 incrementLap()
 
 incrementLap()
+ */
+
+
+/**
+ * PSEUDOCODE
+ * initialize the count as 0
+ * listen for clicks on the increment button
+ * increment the count variable when the button is clicked
+ * change the count-el in the HTML to reflect the new count
+ */
+
+
+let countEl = document.getElementById("count-el")
+console.log(countEl)
+
+let count = 0 
+function increment() {
+    console.log("clicked")
+    count = count + 1
+    console.log(count)
+}
