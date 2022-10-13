ab59db0ba86d60c3c7fdb0d78f3e3a537d1fac78
unknown <adityadenny6@gmail.com>
2022-10-13 21:07:52 +0700

    reassigning and incrementing

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // document.getElementById("count-el").innerText = 7
 
 let count = 0
-let myAge = 31
-let humanDogRatio = 7
-let myDogAge = myAge * humanDogRatio
 
-console.log(myDogAge)
+let bonusPoints = 50
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+bonusPoints = bonusPoints + 45 
+console.log(bonusPoints)
